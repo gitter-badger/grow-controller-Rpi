2aad174719c227cfdaab84e71a61091e3ae12c02
scott snyder <scott@yep.duckdns.org>
2016-06-03 13:09:05 -0700

    Update menu.sh

--- main/menu.sh
+++ main/menu.sh
@@ -6,7 +6,7 @@ fortune | cowsay -f tux
 echo "︻╦╤─--------------------!--------------------─╤╦︻"
 echo "!  TOOLS                 ! RPi Grow controller    !"
 echo "︻╦╤─--------------------!--------------------─╤╦︻"
-echo "! [1] apt-get update     ! [a]                    !"
+echo "! [1] apt-get update     ! [a] Light cycle change !"
 echo "! [2] apt-get upgrade    ! [b] 400HPS on  (GPIO3) !"
 echo "! [3] apt-get dist-up    ! [c] 400HPS off (GPIO3) !"
 echo "! [4] apt-get autoremove ! [d] T5 on      (GPIO4) !"
@@ -39,18 +39,19 @@ case $yourch in
                 read yourch ;;
 	9) tail -f /var/log/syslog && echo -n "Enter to continue"
                 read yourch ;;
-#	a)  ;;
-	b) python /home/pi/HPSon.py && echo -n "Enter to continue"
+	a) python /home/pi/grow-controller-Rpi/main/ref/changeLightCycle.py && echo -n "Enter to continue"
                 read yourch ;;
-	c) python /home/pi/HPSoff.py && echo -n "Enter to continue"
+	b) python /home/pi/grow-controller-Rpi/main/ref/HPSon.py && echo -n "Enter to continue"
                 read yourch ;;
-	d) python /home/pi/FLon.py && echo -n "Enter to continue"
+	c) python /home/pi/grow-controller-Rpi/mainref/HPSoff.py && echo -n "Enter to continue"
                 read yourch ;;
-	e) python /home/pi/FLoff.py && echo -n "Enter to continue"
+	d) python /home/pi/grow-controller-Rpi/main/ref/FLon.py && echo -n "Enter to continue"
                 read yourch ;;
-	f) sudo python /home/pi/dht.py && echo -n "Enter to continue"
+	e) python /home/pi/grow-controller-Rpi/main/ref/FLoff.py && echo -n "Enter to continue"
                 read yourch ;;
-	g) sudo python /home/pi/watchdht.py && echo -n "Enter to continue"
+	f) sudo python /home/pi/grow-controller-Rpi/main/ref/dht.py && echo -n "Enter to continue"
+                read yourch ;;
+	g) sudo python /home/pi/grow-controller-Rpi/main/ref/watchdht.py && echo -n "Enter to continue"
                 read yourch ;;
 	h) tail /var/log/auth.log && echo -n "Enter to continue"
                 read yourch ;;
