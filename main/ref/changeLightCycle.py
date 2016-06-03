5f07cc3f3b71ce3ca50083132b1f8b4f451e42b0
scott snyder <scott@yep.duckdns.org>
2016-06-03 13:11:08 -0700

    Update changeLightCycle.py

--- main/ref/changeLightCycle.py
+++ main/ref/changeLightCycle.py
@@ -9,9 +9,9 @@ print"Ctrl-C to EXIT"
 t = input("12 or 18\n$")
 if t == 12:
         print"light cycle set to\nFLOWER"
-        os.system("crontab /home/pi/grow-controller-Rpi/ref/crontab.flower")#insert command
+        os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower")#insert command
 if t == 18:
         print"light cycle set to\nVEG"
-        os.system("crontab /home/pi/grow-controller-Rpi/ref/crontab.veg")#insert command
+        os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.veg")#insert command
 else:
         exit()
