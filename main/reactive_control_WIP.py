5d11971506d5507369c676816b37a99719c6d2d5
scott snyder <scott@yep.duckdns.org>
2016-06-03 21:12:34 -0700

    Update reactive_control_WIP.py

--- main/reactive_control_WIP.py
+++ main/reactive_control_WIP.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(?, GPIO.OUT) #set relay pins here
+GPIO.setup(22, GPIO.OUT) #set relay pins here
 #h,t = dht.read_retry(dht.DHT22, ?)
 #t = t * 9/5.0 + 32 #convert DHT22 data to F #unreliable find alt. or learn C
 #print t #for testing remove later
@@ -15,12 +15,12 @@ GPIO.setup(?, GPIO.OUT) #set relay pins here
 #temp = (t1 + t2 + t3) / (3) #average multi dht setup
 ##################
 while 1:
-  h,t = dht.read_retry(dht.DHT22, ?) #read DHT22 value, set pin
+  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pin
   time.sleep(1)
   if t > 28:
-    GPIO.output(?, GPIO.HIGH) #side lighting off
+    GPIO.output(22, GPIO.HIGH) #side lighting off
     GPIO.output(?, GPIO.LOW) #emergency vent fan on
     time.sleep(30) #runs emergency vent fan for ()seconds
     GPIO.output(?, GPIO.HIGH) #emergency vent fan off
   if t < 20:
-    GPIO.output(?, GPIO.LOW) #side light on
+    GPIO.output(22, GPIO.LOW) #side light on
