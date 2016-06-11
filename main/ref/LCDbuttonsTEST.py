0393af975c0e9ba10523b6e6d88d8a547310702b
scott snyder <scott@yep.duckdns.org>
2016-06-11 14:56:25 -0700

    created TEST FILE
    BROKEN

---
+++ main/ref/LCDbuttonsTEST.py
@@ -0,0 +1,25 @@
+import time
+import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+
+# Initialize the LCD using the pins
+lcd = LCD.Adafruit_CharLCDPlate()
+
+
+# Make list of button value, text, and backlight color.
+buttons = ( (LCD.SELECT, GPIO.output(18, GPIO.LOW)),
+            (LCD.LEFT,   'Left'  , (1,0,0)),
+            (LCD.UP,     'Up'    , (0,0,1)),
+            (LCD.DOWN,   'Down'  , (0,1,0)),
+            (LCD.RIGHT,  'Right' , (1,0,1)) )
+
+print('Press Ctrl-C to quit.')
+while True:
+    # Loop through each button and check if it is pressed.
+    for button in buttons:
+        if lcd.is_pressed:
+            lcd.clear()
