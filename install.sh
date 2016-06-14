20558fbb25416fef7ce03533e08818fa6d771788
scott snyder <scott@yep.duckdns.org>
2016-06-14 16:49:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git /home/pi &&
 git clone https://github.com/growmaster420/grow-controller-Rpi.git /home/pi &&
 python /home/pi/Adafruit_Python_DHT/setup.py install &&
 python /home/pi/Adafruit_Python_CharLCD/setup.py install &&
-crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower
+crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower &&
+dpkg-reconfigure tzdata
