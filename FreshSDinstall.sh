b79ce1d1a3135933b38a1f67fa08b217aa1afb24
scott snyder <scott@yep.duckdns.org>
2016-06-14 17:18:31 -0700

    Update FreshSDinstall.sh

--- FreshSDinstall.sh
+++ FreshSDinstall.sh
@@ -8,4 +8,4 @@ git clone https://github.com/adafruit/Adafruit_Python_DHT.git  &&
 git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git &&
 git clone https://github.com/growmaster420/grow-controller-Rpi.git &&
 crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower &&
-dpkg-reconfigure tzdata
+sudo dpkg-reconfigure tzdata
