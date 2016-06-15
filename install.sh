5ae4e25d6bfc3b1de49a59ddb83861a9cddc8af9
scott snyder <scott@yep.duckdns.org>
2016-06-14 17:13:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update &&
 sudo apt-get upgrade -y &&
 sudo apt-get dist-upgrade -y &&
 sudo apt-get autoremove -y &&
-sudo apt-get install git python-dev -y &&
+sudo apt-get install build-essential python-dev python-smbus python-pip git -y &&
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git  &&
 git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git &&
 git clone https://github.com/growmaster420/grow-controller-Rpi.git &&
