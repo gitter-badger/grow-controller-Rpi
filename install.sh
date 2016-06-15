400ae4de53ae5e109eb089407d6f9385d9234b41
scott snyder <scott@yep.duckdns.org>
2016-06-14 17:09:39 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ sudo apt-get upgrade -y &&
 sudo apt-get dist-upgrade -y &&
 sudo apt-get autoremove -y &&
 sudo apt-get install git python-dev -y &&
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git /home/pi &&
-git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git /home/pi &&
-git clone https://github.com/growmaster420/grow-controller-Rpi.git /home/pi &&
-python /home/pi/Adafruit_Python_DHT/setup.py install &&
-python /home/pi/Adafruit_Python_CharLCD/setup.py install &&
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git  &&
+git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git &&
+git clone https://github.com/growmaster420/grow-controller-Rpi.git &&
 crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower &&
 dpkg-reconfigure tzdata
