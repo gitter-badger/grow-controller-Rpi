757bc2b0ae35140f9ebba368eb28a9e4824b7530
scott snyder <scott@yep.duckdns.org>
2016-06-15 19:14:00 -0700

    Update LCDbuttonsSELECT.py

--- main/ref/LCDbuttonsSELECT.py
+++ main/ref/LCDbuttonsSELECT.py
@@ -11,5 +11,9 @@ lcd = LCD.Adafruit_CharLCDPlate()
 while True:
     if lcd.is_pressed(LCD.SELECT):
         GPIO.output(18, GPIO.LOW)
+#    if lcd.is_pressed(LCD.UP):
+#    if lcd.is_pressed(LCD.DOWN):
+#    if lcd.is_pressed(LCD.LEFT):
+#    if lcd.is_pressed(LCD.RIGHT):
 
 
