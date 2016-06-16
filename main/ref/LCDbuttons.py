5071130d72740caf3cd209c3eaaa9a1e71571f2f
scott snyder <scott@yep.duckdns.org>
2016-06-15 19:15:47 -0700

    Update and rename LCDbuttonsSELECT.py to LCDbuttons.py

--- main/ref/LCDbuttons.py
+++ main/ref/LCDbuttons.py
@@ -9,10 +9,11 @@ GPIO.setup(18, GPIO.OUT)
 lcd = LCD.Adafruit_CharLCDPlate()
 
 while True:
-    if lcd.is_pressed(LCD.SELECT):
+#    if lcd.is_pressed(LCD.SELECT):
+    if lcd.is_pressed(LCD.UP):
         GPIO.output(18, GPIO.LOW)
-#    if lcd.is_pressed(LCD.UP):
-#    if lcd.is_pressed(LCD.DOWN):
+    if lcd.is_pressed(LCD.DOWN):
+        GPIO.output(18, GPIO.HIGH)
 #    if lcd.is_pressed(LCD.LEFT):
 #    if lcd.is_pressed(LCD.RIGHT):
 
