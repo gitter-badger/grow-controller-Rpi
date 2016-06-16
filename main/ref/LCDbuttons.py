dd9fd84c005dc796b4194bde83c1bdc6f293c1cf
scott snyder <scott@yep.duckdns.org>
2016-06-15 19:48:40 -0700

    Update LCDbuttons.py

--- main/ref/LCDbuttons.py
+++ main/ref/LCDbuttons.py
@@ -9,7 +9,7 @@ GPIO.setup(18, GPIO.OUT)
 lcd = LCD.Adafruit_CharLCDPlate()
 
 while True:
-    time.sleep(0.1) # without this time sleep, 23% cpu usage. with 3%
+    time.sleep(0.1) # without this time.sleep, 23% cpu usage. with 3%
 #    if lcd.is_pressed(LCD.SELECT):
     if lcd.is_pressed(LCD.UP):
         GPIO.output(18, GPIO.LOW)
