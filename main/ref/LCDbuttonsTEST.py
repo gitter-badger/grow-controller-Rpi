876ed7125f38a7dabf5d7e2d2847ae7d30c2dfc8
scott snyder <scott@yep.duckdns.org>
2016-06-15 18:37:04 -0700

    Update LCDbuttonsTEST.py

--- main/ref/LCDbuttonsTEST.py
+++ main/ref/LCDbuttonsTEST.py
@@ -1,27 +1,17 @@
     # Mabey this will work, Test tomorrow durring  SD card redo
     # merge into lcd.py, test before reboot
     # TEST BEFORE REBOOT 
-    #still not tested
-    import time
-    import Adafruit_CharLCD as LCD
-    import RPi.GPIO as GPIO
+import time
+import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+lcd = LCD.Adafruit_CharLCDPlate()
 
-    lcd = LCD.Adafruit_CharLCDPlate()
+while True:
+    if lcd.is_pressed(LCD.SELECT):
+        GPIO.output(18, GPIO.LOW)
 
-    buttons = ( (LCD.SELECT),
-                (LCD.LEFT),
-                (LCD.UP),
-                (LCD.DOWN),
-                (LCD.RIGHT)
-
-    print('Press Ctrl-C to quit.')
-    while True:
-        for button in buttons:
-            if lcd.is_pressed(LCD.SELECT):# its ether this
-#            if lcd.is_pressed(SELECT):# or this, or mabey not who knows
-                GPIO.output(18, GPIO.LOW)
 
