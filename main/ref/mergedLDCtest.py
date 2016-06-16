6e1b7f7dcdbdf1bf768a7b8001436e1d77e8afca
scott snyder <scott@yep.duckdns.org>
2016-06-15 18:40:10 -0700

    Update mergedLDCtest.py

--- main/ref/mergedLDCtest.py
+++ main/ref/mergedLDCtest.py
@@ -8,11 +8,7 @@ GPIO.setmode(GPIO.BCM) #sets the GPIO mode
 GPIO.setwarnings(False) # do not show GPIO warnings
 GPIO.setup(18, GPIO.OUT) # set GPIO 18 as an output
 lcd = LCD.Adafruit_CharLCDPlate() # defines lcd
-buttons = ( (LCD.SELECT),
-            (LCD.LEFT),
-            (LCD.UP),
-            (LCD.DOWN),
-            (LCD.RIGHT)
+
 # the loop
 while True:
   h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
@@ -24,8 +20,7 @@ while True:
     lcd.set_color(1.0, 0.0, 0.0) # red
   if t1 < 80: # explains itself
     lcd.set_color(0.0, 1.0, 0.0) # green
-  for button in buttons: # call the button function
-    if lcd.is_pressed(LCD.SELECT):# its ether this
+  if lcd.is_pressed(LCD.SELECT):# its ether this
 #    if lcd.is_pressed(SELECT):# or this, or mabey not who knows
-      GPIO.output(18, GPIO.LOW) # set GPIO pin 18 low, relay on
+    GPIO.output(18, GPIO.LOW) # set GPIO pin 18 low, relay on
 
