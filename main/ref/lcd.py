b8fd65d56330cc6915e24b9e1227eb150588f407
scott snyder <scott@yep.duckdns.org>
2016-06-15 18:53:17 -0700

    Update lcd.py

--- main/ref/lcd.py
+++ main/ref/lcd.py
@@ -1,20 +1,25 @@
-# using https://www.adafruit.com/products/1110 lcd
-# buttons need to do something
-# This file gets invoked by Cron, installed by changing light cycle in menu
-
 import Adafruit_CharLCD as LCD
 import Adafruit_DHT as dht
+import RPi.GPIO as GPIO
 import time
+# the setup
+GPIO.setmode(GPIO.BCM) #sets the GPIO mode
+GPIO.setwarnings(False) # do not show GPIO warnings
+GPIO.setup(18, GPIO.OUT) # set GPIO 18 as an output
+lcd = LCD.Adafruit_CharLCDPlate() # defines lcd
 
-lcd = LCD.Adafruit_CharLCDPlate()
-
+# the loop
 while True:
-  h,t = dht.read_retry(dht.DHT22, 17)
-  t1 = t * 9/5.0 + 32
-  time.sleep(10)
-  lcd.clear()
-  lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h))
-  if t1 > 80:
-    lcd.set_color(1.0, 0.0, 0.0)#red
-  if t1 < 80:
-    lcd.set_color(0.0, 1.0, 0.0)#green
+  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
+  t1 = t * 9/5.0 + 32 # Convert to F
+  if lcd.is_pressed(LCD.SELECT):# its ether this
+    GPIO.output(18, GPIO.LOW) # set GPIO pin 18 low, relay on
+  time.sleep(3) #sleep for 10 sec
+  lcd.clear() # clear the lcd
+  lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)) # print the DHT22 values in the lcd
+  if t1 > 80: # explains itself
+    lcd.set_color(1.0, 0.0, 0.0) # red
+  if t1 < 80: # explains itself
+    lcd.set_color(0.0, 1.0, 0.0) # green
+
+
