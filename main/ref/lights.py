28484de9d7d7063aef089c9118dc5757f79c368d
scott snyder <scott@yep.duckdns.org>
2016-06-17 10:28:54 -0700

    Create lights.py

---
+++ main/ref/lights.py
@@ -0,0 +1,19 @@
+
+
+print "︻╦╤─----------------─╤╦︻"
+print "!  TOOLS                 !"
+print "︻╦╤─----------------─╤╦︻"
+print "! [1] Light cycle change !"
+print "! [2] 400HPS on          !"
+print "! [3] 400HPS off         !"
+print "! [4] T5 on              !"
+print "! [5] T5 off             !"
+print "! [6] wiring light on    !"
+print "! [7] wiring light off   !"
+print "! [8] fans on            !"
+print "! [9] fans off           !"
+print "! [0] Exit               !"
+print "︻╦╤─----------------─╤╦︻"
+var = input("[1-0,a-k]: ")
+if var == 1:
+  
