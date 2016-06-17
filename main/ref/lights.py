68bbb538713c83c254ac339f8469cfa71ccff48d
scott snyder <scott@yep.duckdns.org>
2016-06-17 11:10:09 -0700

    Update lights.py

--- main/ref/lights.py
+++ main/ref/lights.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-
+import os
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
@@ -7,21 +7,23 @@ GPIO.setup(27, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
-print "︻╦╤─----------------─╤╦︻"
-print "!  TOOLS                 !"
-print "︻╦╤─----------------─╤╦︻"
-print "! [1]                   !"
-print "! [2] 400HPS on          !"
-print "! [3] 400HPS off         !"
-print "! [4] T5 on              !"
-print "! [5] T5 off             !"
-print "! [6] wiring light on    !"
-print "! [7] wiring light off   !"
-print "! [8] fans on            !"
-print "! [9] fans off           !"
-print "! [0] Exit               !"
-print "︻╦╤─----------------─╤╦︻"
+
 while True:
+  os.system('clear')
+  print "!------------------------!"
+  print "!  TOOLS                 !"
+  print "!------------------------!"
+  print "! [1]                    !"
+  print "! [2] 400HPS on          !"
+  print "! [3] 400HPS off         !"
+  print "! [4] T5 on              !"
+  print "! [5] T5 off             !"
+  print "! [6] wiring light on    !"
+  print "! [7] wiring light off   !"
+  print "! [8] fans on            !"
+  print "! [9] fans off           !"
+  print "! [0] Exit               !"
+  print "!------------------------!"
   var = input("[1-0,a-k]: ")
   if var == 1:
   elif var == 2:
