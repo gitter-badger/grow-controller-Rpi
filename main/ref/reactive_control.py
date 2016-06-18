d5eebebd8908e1fbef2e019a002c550adaa78e6e
scott snyder <scott@yep.duckdns.org>
2016-06-17 23:50:20 -0700

    Update reactive_control.py

--- main/ref/reactive_control.py
+++ main/ref/reactive_control.py
@@ -8,7 +8,7 @@ GPIO.setwarnings(False)
 GPIO.setup(22, GPIO.OUT) #side lighting
 GPIO.setup(23, GPIO.OUT) #fan
 while True:
-  time.sleep(30)
+  time.sleep(60)
   h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pin
   time.sleep(1)
   t1 = t * 9/5.0 + 32
