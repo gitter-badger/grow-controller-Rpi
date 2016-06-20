99fac2f47849de23e2f5a4a1eeb9855fb0908ac4
scott snyder <scott@yep.duckdns.org>
2016-06-19 22:50:16 -0700

    Update reactive_control.py

--- main/ref/reactive_control.py
+++ main/ref/reactive_control.py
@@ -7,6 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(22, GPIO.OUT) #side lighting
 GPIO.setup(23, GPIO.OUT) #fan
+#GPIO.setup(?, GPIO.OUT) #heater
 while True:
   time.sleep(60)
   h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pin
@@ -18,4 +19,5 @@ while True:
     GPIO.output(23, GPIO.LOW) #fan on
   elif t1 < 68:
     if t1 > 66:
-      GPIO.output(23, GPIO.HIGH) #fan off
+      GPIO.output(?, GPIO.LOW) #heater on
+      GPIO.output(?, GPIO.HIGH ) #heater off
