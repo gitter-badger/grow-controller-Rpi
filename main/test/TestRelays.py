2179fc691ee11100651a07afef2d9adcf6307018
scott snyder <scott@yep.duckdns.org>
2016-06-23 10:44:50 -0700

    Create TestRelays.py

---
+++ main/test/TestRelays.py
@@ -0,0 +1,12 @@
+import sys, getopt
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)#FL
+GPIO.setup(27, GPIO.OUT)#HPS
+
+if opt == '-hps':
+  GPIO.output(18, GPIO.HIGH)#on
