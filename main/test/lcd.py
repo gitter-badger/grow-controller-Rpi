15e4ca01f79986df3725da1ba2217adca57341f7
scott snyder <scott@yep.duckdns.org>
2016-06-22 19:31:07 -0700

    Create lcd.py

---
+++ main/test/lcd.py
@@ -0,0 +1,22 @@
+#
+import Adafruit_CharLCD as LCD
+import Adafruit_DHT as dht
+import time
+
+lcd = LCD.Adafruit_CharLCDPlate() # defines lcd
+
+def 
+
+while True:
+  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
+  t1 = t * 9/5.0 + 32 # Convert to F
+  time.sleep(10) #sleep for 10 sec
+  lcd.clear() # clear the lcd
+  lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)) # print the DHT22 values in the lcd
+  if t1 > 80: # explains itself
+    lcd.set_color(1.0, 0.0, 0.0) # red = too hot turn off extra lighting, fan at max
+  if t1 < 80: # explains itself
+    if t1 > 69:
+      lcd.set_color(0.0, 1.0, 0.0) # green = good temp
+  if t1 < 69: 
+    lcd.set_color(0.0, 0.0, 1.0) # Blue = cold shut off a/c
