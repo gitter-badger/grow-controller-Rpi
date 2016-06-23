69cc9fc55aaa2add6f2a6484f55eb29ebead66a0
scott snyder <scott@yep.duckdns.org>
2016-06-22 20:27:02 -0700

    Update watchdht.py

--- main/ref/watchdht.py
+++ main/ref/watchdht.py
@@ -3,10 +3,14 @@ import time
 import os
 import datetime
 
+def temp():
+  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pi
+  t1 = t * 9/5.0 + 32
+  print datetime.datetime.now().strftime("%H:%M:%S")
+  print 'T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)
+
+
 while True:
-        h,t = dht.read_retry(dht.DHT22, 17)
         time.sleep(8.0)
-        t1 = t * 9/5.0 + 32
         os.system('clear')
-        print datetime.datetime.now().strftime("%H:%M:%S")
-        print 'T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)
+        temp()
