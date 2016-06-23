190f719da060a7d4c69470cbcb50f0c74312d374
scott snyder <scott@yep.duckdns.org>
2016-06-22 20:39:54 -0700

    Update lcd.py

--- main/test/lcd.py
+++ main/test/lcd.py
@@ -11,17 +11,18 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 
 def lcdDis():
-  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
-  t1 = t * 9/5.0 + 32 # Convert to F
-  lcd.clear() # clear the lcd
-  lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)) # print the DHT22 values in the lcd
-  if t1 > 80: # explains itself
-    lcd.set_color(1.0, 0.0, 0.0) # red = too hot turn off extra lighting, fan at max
-  if t1 > 69:
+  while True:
+    h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
+    t1 = t * 9/5.0 + 32 # Convert to F
+    lcd.clear() # clear the lcd
+    lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)) # print the DHT22 values in the lcd
+    if t1 > 80: # explains itself
+      lcd.set_color(1.0, 0.0, 0.0) # red = too hot turn off extra lighting, fan at max
+    if t1 > 69:
       lcd.set_color(0.0, 1.0, 0.0) # green = good temp
-  if t1 < 69: 
+    if t1 < 69: 
      lcd.set_color(0.0, 0.0, 1.0) # Blue = cold shut off a/c
-  time.sleep(10)
+    time.sleep(10)
   
 def lcdBut():
   while True:
