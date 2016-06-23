839a16492544f84c1ba114497690fdbf3d381ca6
scott snyder <scott@yep.duckdns.org>
2016-06-23 14:56:26 -0700

    Update TestRelays.py

--- main/test/TestRelays.py
+++ main/test/TestRelays.py
@@ -1,14 +1,34 @@
-import sys, getopt
+#trying to do commandline arguments
+import argparse
 import RPi.GPIO as GPIO
-
+parser = argparse.ArgumentParser()
+parser.add_argument("-won")#wiring lights
+parser.add_argument("-woff")
+args = parser.parse_args()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(23, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)#wiring light
+GPIO.setup(23, GPIO.OUT)#fans
 GPIO.setup(22, GPIO.OUT)#FL
 GPIO.setup(27, GPIO.OUT)#HPS
-
-if opt == '-h':
-  GPIO.output(18, GPIO.HIGH)#on
-  exit
+def wlon():
+  GPIO.output(18, GPIO.LOW)#on
+def woff():
+  GPIO.output(18, GPIO.HIGH)#off
   
+if args.won:
+  wlon()
+  exit()
+elif args.woff:
+  woff()
+  exit()
+
+
+
+
+
+
+
+
+
+
