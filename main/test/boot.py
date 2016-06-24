8d1d0c6b186ea0d0bb36dff1d0e846dd42b3778f
scott snyder <scott@yep.duckdns.org>
2016-06-24 15:42:55 -0700

    Update boot.py

--- main/test/boot.py
+++ main/test/boot.py
@@ -1,1 +1,16 @@
 # boot set default relay values
+
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)#wiring light
+GPIO.setup(23, GPIO.OUT)#fans
+GPIO.setup(22, GPIO.OUT)#FL
+GPIO.setup(27, GPIO.OUT)#HPS
+GPIO.output(18, GPIO.LOW)#on
+GPIO.output(23, GPIO.LOW)#on
+GPIO.output(22, GPIO.LOW)#on
+GPIO.output(27, GPIO.HIGH)#off
+exit()
