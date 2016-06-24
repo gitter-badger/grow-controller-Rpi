b18359e68f9f845f51208361ea049b3383174ccf
scott snyder <scott@yep.duckdns.org>
2016-06-23 23:05:09 -0700

    Update changeLightCycle.py

--- main/ref/changeLightCycle.py
+++ main/ref/changeLightCycle.py
@@ -2,19 +2,22 @@
 #
 #
 import os
-
 print"Set light cycle"
 print"!!!! WARNING  REPLACES   CURRENT  CRONTAB !!!!"
 print"!!!! CHECK YOUR CRONTAB WITH $ crontab -l !!!!"
 print"Ctrl-C to EXIT"
 print"12 is flower ON=22:00 OFF=10:00"
 print"18 is veg ON=19:00 OFF=13:00"
-t = input("12 or 18\n$")
+print"2 = two room setup"
+t = input("12 or 18 or 2\n$")
 if t == 12:
-        print"light cycle set to\nFLOWER"
-        os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower")
-if t == 18:
-        print"light cycle set to\nVEG"
-        os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.veg")
+  print"light cycle set to\nFLOWER"
+  os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower")
+elif t == 18:
+  print"light cycle set to\nVEG"
+  os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.veg")
+elif t == 2:
+  print"light cycle set to\nTwo Rooms, One Grow Controller"
+  os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.tworoom")
 else:
-        exit()
+  exit()
