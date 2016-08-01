6cb8fb617d7823612d4a0646bd5240ca04750d93
scott snyder <scott@growmaster420.com>
2016-07-31 21:59:03 -0700

    Update boot.py

--- main/ref/boot.py
+++ main/ref/boot.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)#wiring light
-GPIO.setup(23, GPIO.OUT)#fans
-GPIO.setup(22, GPIO.OUT)#FL
+GPIO.setup(18, GPIO.OUT)#
+GPIO.setup(23, GPIO.OUT)#
+GPIO.setup(22, GPIO.OUT)#
 GPIO.setup(27, GPIO.OUT)#HPS
 
 GPIO.output(18, GPIO.HIGH)#off
-GPIO.output(23, GPIO.LOW)#on
-GPIO.output(22, GPIO.LOW)#on
+GPIO.output(23, GPIO.HIGH)#off
+GPIO.output(22, GPIO.HIGH)#off
 GPIO.output(27, GPIO.HIGH)#off
 exit()
