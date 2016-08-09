f23757849922513a4203c1f342bde583c43c1c45
scott snyder <scott@growmaster420.com>
2016-08-08 23:33:34 -0700

    Update lcd.py

--- main/ref/lcd.py
+++ main/ref/lcd.py
@@ -16,18 +16,21 @@ GPIO.setup(22, GPIO.OUT) #22 = veg light
 def lcdDis(): #display function
   while True:
     h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
-    t1 = t * 9/5.0 + 32 
-    time.sleep(0.5)
-    lcd.clear() # clear the lcd
-    lcd.message('T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)) # print the DHT22 values in the lcd
-    if t1 > 69:
-      if t1 < 80:
+    time.sleep(1)
+    t1 = t * 9/5.0 + 32
+
+    if t > 20:
+      if t < 29:
         lcd.set_color(0.0, 1.0, 0.0) # green = good temp
-    elif t1 > 80: # explains itself
+    elif t > 29: # explains itself
       lcd.set_color(1.0, 0.0, 0.0) # red = too hot turn off extra lighting, fan at max
-    elif t1 < 69: 
+    elif t < 20:
      lcd.set_color(0.0, 0.0, 1.0) # Blue = cold shut off a/c
-  
+
+    time.sleep(1)
+    lcd.clear() # clear the lcd
+    lcd.message('T={0:0.1f} H={1:0.1f}\n F={2:0.1f}'.format(t, h, t1)) # print the DHT22 values in the lcd
+
 def lcdBut(): #button functions
   while True:
     time.sleep(0.1) # without this time.sleep, 23% cpu usage. with 3%
@@ -40,8 +43,10 @@ def lcdBut(): #button functions
       GPIO.output(22, GPIO.LOW)
     elif lcd.is_pressed(LCD.RIGHT):
       GPIO.output(22, GPIO.HIGH)
-  
+
 
 if __name__ == '__main__': #run the above functions in the background, FOREVER!!!!!!!!!!!!!!!!!!!!!
   Process(target=lcdDis).start()
   Process(target=lcdBut).start()
+
+
