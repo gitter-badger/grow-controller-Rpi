793f682562d2d447ba4ff62bce7f219645d91251
scott snyder <scott@growmaster420.com>
2016-10-06 13:29:47 -0700

    Update boot.py

--- main/ref/boot.py
+++ main/ref/boot.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)#
-GPIO.setup(23, GPIO.OUT)#
-GPIO.setup(22, GPIO.OUT)#
+GPIO.setup(18, GPIO.OUT)#water pump
+GPIO.setup(23, GPIO.OUT)#oc fan
+GPIO.setup(22, GPIO.OUT)#ballast fan
 GPIO.setup(27, GPIO.OUT)#HPS
 
 GPIO.output(18, GPIO.HIGH)#off
-GPIO.output(23, GPIO.HIGH)#off
+GPIO.output(23, GPIO.LOW)#on
 GPIO.output(22, GPIO.HIGH)#off
 GPIO.output(27, GPIO.HIGH)#off
 exit()
