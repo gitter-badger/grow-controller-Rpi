28a9317920c8acc71d2903affac9af7080fa54ef
scott snyder <scott@growmaster420.com>
2016-10-15 13:54:27 -0700

    Add files via upload

---
+++ main/ref/calu.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('clear')
+print 'Area calculator\n'
+print "enter height"
+varH = int(input())
+print 'enter width'
+varW = int(input())
+print 'enter depth'
+varD = int(input())
+print 'calculating Volume'
+print (varD * varH * varW)
+print 'calculating Area'
+print (varW * varD)
