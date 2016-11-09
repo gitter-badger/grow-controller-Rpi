5bb480a214e45c66acd6d45330f6e53d9f7fb454
scott snyder <scott@growmaster420.com>
2016-11-09 11:23:22 -0800

    Update changeLightCycle.py

--- main/ref/changeLightCycle.py
+++ main/ref/changeLightCycle.py
@@ -9,15 +9,25 @@ print"Ctrl-C to EXIT"
 print"12 is flower ON=11:00 OFF=23:00"
 print"18 is veg ON=19:00 OFF=13:00"
 print"2 = two room setup"
+print"e, to edit"
 t = input("12 or 18 or 2\n$")
 if t == 12:
   print"light cycle set to\nFLOWER"
   os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower")
-elif t == 18:
+if t == 18:
   print"light cycle set to\nVEG"
   os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.veg")
-elif t == 2:
+if t == 2:
   print"light cycle set to\nTwo Rooms, One Grow Controller"
   os.system("crontab /home/pi/grow-controller-Rpi/main/ref/crontab.tworoom")
+if t == e:
+  print"select file to edit; 12,18,or 2 "
+  s = input("12, 18 or 2")
+  if s == 12:
+    os.system("nano /home/pi/grow-controller-Rpi/main/ref/crontab.flower")
+  if s == 18:
+    os.system("nano /home/pi/grow-controller-Rpi/main/ref/crontab.veg")
+  if s == 2:
+    os.system("nano /home/pi/grow-controller-Rpi/main/ref/crontab.tworoom")  
 else:
   exit()
