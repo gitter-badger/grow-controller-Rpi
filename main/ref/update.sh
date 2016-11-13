ea28d0de00ef6472417e44fb33fcefcba8f69234
scott snyder <scott@growmaster420.com>
2016-11-12 17:19:44 -0800

    Update update.sh

--- main/ref/update.sh
+++ main/ref/update.sh
@@ -1,1 +1,18 @@
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+command -v apt >/dev/null 2>&1 || { echo "apt is not installed. not ubuntu?" >&2; exit 0; }
+echo "want to install anything?: [y/n]"  
+read yourch
+apt update
+if [ $yourch = "y" ];
+  then
+    echo "what program would u like to install"
+    read insch
+    apt install $insch -y
+    apt upgrade -y
+    apt dist-upgrade -y
+    apt autoremove -y
+else
+    apt upgrade -y
+    apt dist-upgrade -y
+    apt autoremove -y
+fi
+exit
