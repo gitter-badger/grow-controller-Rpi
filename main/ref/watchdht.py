4df7cd2c6ba5470aa377121eb490ef3bba3051c7
scott snyder <scott@growmaster420.com>
2016-11-24 18:17:30 -0800

    Update watchdht.py

--- main/ref/watchdht.py
+++ main/ref/watchdht.py
@@ -5,10 +5,8 @@ import datetime
 
 def temp():
   h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pi
-  t1 = t * 9/5.0 + 32
   print datetime.datetime.now().strftime("%H:%M:%S")
-  print 'T={0:0.1f}*F\nH={1:0.1f}%'.format(t1, h)
-
+  print 'T={0:0.1f}*C\nH={1:0.1f}%'.format(t, h)
 
 while True:
   time.sleep(8.0)
