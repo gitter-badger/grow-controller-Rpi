e0f0e2dd605f9daf7ea4203154f12295a6f64a80
scott snyder <scott@growmaster420.com>
2016-12-11 14:07:54 -0800

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#installer
+sudo apt update
+sudo apt upgrade -y
+sudo apt install 
+sudo apt-get install build-essential python-dev python-smbus python-pip git cowsay fortune rpi-update -y
+sudo rpi-update
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git /home/pi
+git clone https://github.com/adafruit/Adafruit_Python_CharLCD.git /home/pi
+sudo python /home/pi/Adafruit_Python_DHT/setup.py install 
+sudo python /home/pi/Adafruit_Python_CharLCD/setup.py install 
+git clone https://github.com/growmaster420/grow-controller-Rpi.git /home/pi
+sudo dpkg-reconfigure tzdata
+cat /home/pi/grow-controller-Rpi/.bashrc > /home/pi/.bashrc
+crontab /home/pi/grow-controller-Rpi/main/ref/crontab.flower
+sudo raspi-config #i2c enable
+
