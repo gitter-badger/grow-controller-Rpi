8ebff638dc2c5a26776450dabc344acd1bfbd656
scott snyder <scott@growmaster420.com>
2016-12-27 19:45:17 -0800

    Update lcd.py

--- main/ref/lcd.py
+++ main/ref/lcd.py
@@ -3,35 +3,32 @@
 # buttons currently not in use
 # more intergrations into this file must be made
 from multiprocessing import Process
-import RPi.GPIO as GPIO
+
 import Adafruit_CharLCD as LCD
 import Adafruit_DHT as dht
 import Adafruit_DHT
-
+import settings
 from datetime import datetime
 import time
-
+settings.temps()
 lcd = LCD.Adafruit_CharLCDPlate() # defines lcd
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT) #18 = wiring light 
-GPIO.setup(22, GPIO.OUT) #22 = veg light
-
+temp1 = settings.maxTemp
+temp2 = settings.minTemp
 def lcdDis(): #display function
   while True:
     now = datetime.now()
     h,t = dht.read_retry(dht.DHT22, 17) #read DHT22
     time.sleep(1)
     t1 = t * 9/5.0 + 32
-    if t > 20:
-      if t < 35:
+    if t > temp2:
+      if t < temp1:
         lcd.set_color(0.0, 1.0, 0.0) # green = good temp
-    if t > 35: # explains itself
+    if t > temp1: # explains itself
       lcd.set_color(1.0, 0.0, 0.0) # red = too hot turn off extra lighting, fan at max
       lcd.clear()
       lcd.message(' TEMP TOO HIGH \n')
       time.sleep(5)
-    if t < 20:
+    if t < temp2:
       lcd.set_color(0.0, 0.0, 1.0) # Blue = cold shut off a/c
       lcd.message('TEMP TOO LOW\n')
       time.sleep(5)
@@ -47,13 +44,13 @@ def lcdBut(): #button functions !!!NOT IN USE!!!
     time.sleep(0.1) # without this time.sleep, 23% cpu usage. with 3%
     if lcd.is_pressed(LCD.SELECT):
     if lcd.is_pressed(LCD.UP):
-      GPIO.output(18, GPIO.LOW)
+      
     elif lcd.is_pressed(LCD.DOWN):
-      GPIO.output(18, GPIO.HIGH)
+     
     elif lcd.is_pressed(LCD.LEFT):
-      GPIO.output(22, GPIO.LOW)
+      
     elif lcd.is_pressed(LCD.RIGHT):
-      GPIO.output(22, GPIO.HIGH)
+      
 '''
 
 if __name__ == '__main__': #run the above functions in the background, FOREVER!!!!!!!!!!!!!!!!!!!!!
