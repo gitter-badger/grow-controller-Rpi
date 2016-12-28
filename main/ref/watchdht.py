7db8a5a34c492eb668a2699b0e0b53cbd209c061
scott snyder <scott@growmaster420.com>
2016-12-28 02:13:47 -0800

    Update watchdht.py

--- main/ref/watchdht.py
+++ main/ref/watchdht.py
@@ -2,13 +2,14 @@ import Adafruit_DHT as dht
 import time
 import os
 import datetime
-
-def temp():
-  h,t = dht.read_retry(dht.DHT22, 17) #read DHT22 value, set pi
-  print datetime.datetime.now().strftime("%H:%M:%S")
-  print 'T={0:0.1f}*C\nH={1:0.1f}%'.format(t, h)
+import settings
+settings.pins()
+pin5 = settings.dhtsensor
 
 while True:
+  h,t = dht.read_retry(dht.DHT22, pin5) #read DHT22 value, set pi
+  print datetime.datetime.now().strftime("%H:%M:%S")
+  print 'T={0:0.1f}*C\nH={1:0.1f}%'.format(t, h)
   time.sleep(8.0)
   os.system('clear')
   temp()
