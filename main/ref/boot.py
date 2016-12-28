c5be8a93acde0441ac4cc62e1b08ad90b3c0c8d8
scott snyder <scott@growmaster420.com>
2016-12-27 21:49:26 -0800

    Update boot.py

--- main/ref/boot.py
+++ main/ref/boot.py
@@ -1,15 +1,24 @@
 # boot set default relay values
 import RPi.GPIO as GPIO
+import settings
 
+settings.pins()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)#water pump
-GPIO.setup(23, GPIO.OUT)#oc fan
-GPIO.setup(22, GPIO.OUT)#ballast fan
-GPIO.setup(27, GPIO.OUT)#HPS
 
-GPIO.output(18, GPIO.HIGH)#off
-GPIO.output(23, GPIO.LOW)#on
-GPIO.output(22, GPIO.HIGH)#off
-GPIO.output(27, GPIO.HIGH)#off
+pin1 = settings.ballast
+pin2 = settings.ballastfan
+pin3 = settings.heater
+pin4 = settings.ocfan
+
+GPIO.setup(pin1, GPIO.OUT)#HPS
+GPIO.setup(pin2, GPIO.OUT)#ballast fan
+GPIO.setup(pin3, GPIO.OUT)#water pump
+GPIO.setup(pin4, GPIO.OUT)#oc fan
+
+GPIO.output(pin1, GPIO.HIGH)#off
+GPIO.output(pin2, GPIO.HIGH)#off
+GPIO.output(pin3, GPIO.HIGH)#off
+GPIO.output(pin4, GPIO.LOW)#on
+
 exit()
