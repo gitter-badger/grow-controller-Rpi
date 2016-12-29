3068648d06a402a457121654987b3b823cf258f7
scott snyder <scott@growmaster420.com>
2016-12-28 20:41:53 -0800

    Create cronmaker.py

---
+++ main/ref/cronmaker.py
@@ -0,0 +1,20 @@
+#import os
+import settings
+settings.pins()
+settings.light()
+pin1 = settings.ballast
+pin2 = settings.ballastfan
+pin3 = settings.heater
+pin4 = settings.ocfan
+lightOn = settings.lightOn
+lightOff = settings.lightOff
+print ("@reboot python /home/pi/grow-controller-Rpi/main/ref/boot.py")
+print ("@reboot python /home/pi/grow-controller-Rpi/main/ref/lcd.py &")
+print ("@reboot sudo echo", pin1, "/sys/class/gpio/export")
+print ("@reboot sudo echo", pin2, "/sys/class/gpio/export")
+print ("@reboot sudo echo", pin3, "/sys/class/gpio/export")
+print ("@reboot sudo echo", pin4, "/sys/class/gpio/export")
+print("0", lightOn, "0 0 0 python /home/pi/grow-controller-Rpi/main/ref/relay.py -m on")
+print("0", lightOff, "0 0 0 python /home/pi/grow-controller-Rpi/main/ref/relay.py -m off")
+print("0", lightOn, "0 0 0 python /home/pi/grow-controller-Rpi/main/ref/relay.py -b on")
+print("0", lightOff, "0 0 0 python /home/pi/grow-controller-Rpi/main/ref/relay.py -b off")
