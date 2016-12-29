4a7c00a646bbd93c6f8a5f8a5f0d8721955e59d5
scott snyder <scott@growmaster420.com>
2016-12-28 21:06:39 -0800

    Update gpioState.py

--- main/ref/gpioState.py
+++ main/ref/gpioState.py
@@ -12,31 +12,34 @@ pin1 = settings.ballast
 pin2 = settings.ballastfan
 pin3 = settings.heater
 pin4 = settings.ocfan
-
+pinvar1 = ("/sys/class/gpio/gpio", pin1"/value")
+pinvar2 = ("/sys/class/gpio/gpio", pin2"/value")
+pinvar3 = ("/sys/class/gpio/gpio", pin3"/value")
+pinvar4 = ("/sys/class/gpio/gpio", pin4"/value")
 
 def relay1():
-  with open("/sys/class/gpio/gpio", pin1"/value") as gpio1:
+  with open(pinvar1) as gpio1:
     var1 = int(gpio1.read())
   if var1 == 0:
     print "on"
   elif var1 == 1:
     print "off"
 def relay2():
-  with open("/sys/class/gpio/gpio", pin2"/value") as gpio2:
+  with open(pinvar2) as gpio2:
     var2 = int(gpio2.read())
   if var2 == 0:
     print "on"
   elif var2 == 1:
     print "off"
 def relay3():
-  with open("/sys/class/gpio/gpio", pin3"/value") as gpio3:
+  with open(pinvar3) as gpio3:
     var3 = int(gpio3.read())
   if var3 == 0:
     print "on"
   elif var3 == 1:
     print "off"
 def relay4():
-  with open("/sys/class/gpio/gpio", pin4"/value") as gpio4:
+  with open(pinvar4) as gpio4:
     var4 = int(gpio4.read())
   if var4 == 0:
     print "on"
