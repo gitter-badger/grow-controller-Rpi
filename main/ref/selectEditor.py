02b295a58df872a828e0184788afed412e57548c
scott snyder <scott@growmaster420.com>
2016-12-28 23:46:09 -0800

    lols

---
+++ main/ref/selectEditor.py
@@ -0,0 +1,11 @@
+while True:
+    print("Select Default Editor")
+    print("1) emacs")
+    print("2) nano")
+    print("1) Vim/Vi")
+    var1 = input("[1,2,3]")
+    if (var1 != "2"):
+        print("try again")
+    if (var1 == "2"):
+        print("good choice")
+        exit()
