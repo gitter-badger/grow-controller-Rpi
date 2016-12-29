71a0908a60d5f275463101b7df79b00a03b8f541
scott snyder <scott@growmaster420.com>
2016-12-29 13:07:22 -0800

    Update settings.py

--- main/ref/settings.py
+++ main/ref/settings.py
@@ -13,10 +13,10 @@ def pins():
     dhtsensor = 17
 def expGpio():
     global gp1, gp2, gp3, gp4
-    gp1 = "/sys/class/gpio/gpio18/value"
+    gp1 = "/sys/class/gpio/gpio27/value"
     gp2 = "/sys/class/gpio/gpio22/value"
-    gp3 = "/sys/class/gpio/gpio23/value"
-    gp4 = "/sys/class/gpio/gpio27/value"
+    gp3 = "/sys/class/gpio/gpio18/value"
+    gp4 = "/sys/class/gpio/gpio23/value"
 
 ###set light times here###
 def light():
