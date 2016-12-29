4c80d7f96782f813bbc730d8aac90280965e55f8
scott snyder <scott@growmaster420.com>
2016-12-29 13:12:30 -0800

    Update gpioState.py

--- main/ref/gpioState.py
+++ main/ref/gpioState.py
@@ -10,39 +10,39 @@ def relay1():
   with open(pinvar1) as gpio1:
     var1 = int(gpio1.read())
   if var1 == 0:
-    print "on"
+    print("on")
   elif var1 == 1:
-    print "off"
+    print("off")
 def relay2():
   with open(pinvar2) as gpio2:
     var2 = int(gpio2.read())
   if var2 == 0:
-    print "on"
+    print("on")
   elif var2 == 1:
-    print "off"
+    print("off")
 def relay3():
   with open(pinvar3) as gpio3:
     var3 = int(gpio3.read())
   if var3 == 0:
-    print "on"
+    print("on")
   elif var3 == 1:
-    print "off"
+    print("off")
 def relay4():
   with open(pinvar4) as gpio4:
     var4 = int(gpio4.read())
   if var4 == 0:
-    print "on"
+    print("on")
   elif var4 == 1:
-    print "off"
+    print("off")
 
 
     
 if __name__ == "__main__":
-  print "relay1"
+  print("relay1")
   relay1()
-  print "relay2"
+  print("relay2")
   relay2()
-  print "relay3"
+  print("relay3")
   relay3()
-  print "relay4"
+  print("relay4")
   relay4()
