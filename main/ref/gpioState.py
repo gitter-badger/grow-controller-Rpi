c4a9d12c2f8c4e9c3c1b323409b7fedb47e4107c
scott snyder <scott@growmaster420.com>
2016-12-28 21:03:22 -0800

    Update gpioState.py

--- main/ref/gpioState.py
+++ main/ref/gpioState.py
@@ -13,37 +13,36 @@ pin2 = settings.ballastfan
 pin3 = settings.heater
 pin4 = settings.ocfan
 
+
 def relay1():
-  with open('/sys/class/gpio/gpio', pin1'/value') as gpio1:
+  with open("/sys/class/gpio/gpio", pin1"/value") as gpio1:
     var1 = int(gpio1.read())
   if var1 == 0:
     print "on"
   elif var1 == 1:
     print "off"
-
 def relay2():
-  with open('/sys/class/gpio/gpio', pin2'/value') as gpio2:
+  with open("/sys/class/gpio/gpio", pin2"/value") as gpio2:
     var2 = int(gpio2.read())
   if var2 == 0:
     print "on"
   elif var2 == 1:
     print "off"
-
 def relay3():
-  with open('/sys/class/gpio/gpio', pin3'/value') as gpio3:
+  with open("/sys/class/gpio/gpio", pin3"/value") as gpio3:
     var3 = int(gpio3.read())
   if var3 == 0:
     print "on"
   elif var3 == 1:
     print "off"
-
 def relay4():
-  with open('/sys/class/gpio/gpio', pin4'/value') as gpio4:
+  with open("/sys/class/gpio/gpio", pin4"/value") as gpio4:
     var4 = int(gpio4.read())
   if var4 == 0:
     print "on"
   elif var4 == 1:
     print "off"
+
 
     
 if __name__ == "__main__":
