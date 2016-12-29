21954e2936f08b13f0df68821dbebb28bb325b7b
scott snyder <scott@growmaster420.com>
2016-12-29 02:04:56 -0800

    Update selectEditor.py

--- main/ref/selectEditor.py
+++ main/ref/selectEditor.py
@@ -3,7 +3,7 @@ while True:
     print("1) emacs")
     print("2) nano")
     print("1) Vim/Vi")
-    var1 = input("[1,2,3]")
+    var1 = input("[1,2,3]:\n")
     if (var1 != "2"):
         print("try again")
     if (var1 == "2"):
