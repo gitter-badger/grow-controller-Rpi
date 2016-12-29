5606e3256dfb30304718d1ea3986c28b9b9b64d2
scott snyder <scott@growmaster420.com>
2016-12-28 20:56:59 -0800

    Update gpioState.py

--- main/ref/gpioState.py
+++ main/ref/gpioState.py
@@ -2,45 +2,52 @@
 # relay menu depends on this for state, must update for future changes
 ### file depends on crontab opening system gpio
 
-def relay18():
-  with open('/sys/class/gpio/gpio18/value') as gpio18:
-    var18 = int(gpio18.read())
-  if var18 == 0:
+
+
+import settings
+
+settings.pins()
+
+pin1 = settings.ballast
+pin2 = settings.ballastfan
+pin3 = settings.heater
+pin4 = settings.ocfan
+
+def relay1():
+  with open('/sys/class/gpio/gpio', pin1'/value') as gpio1:
+    var1 = int(gpio1.read())
+  if var1 == 0:
     print "on"
-  elif var18 == 1:
+  elif var1 == 1:
     print "off"
 
-def relay22():
-  with open('/sys/class/gpio/gpio22/value') as gpio22:
-    var22 = int(gpio22.read())
-  if var22 == 0:
+def relay2():
+  with open('/sys/class/gpio/gpio', pin2'/value') as gpio2:
+    var2 = int(gpio2.read())
+  if var2 == 0:
     print "on"
-  elif var22 == 1:
+  elif var2 == 1:
     print "off"
 
-def relay23():
-  with open('/sys/class/gpio/gpio23/value') as gpio23:
-    var23 = int(gpio23.read())
-  if var23 == 0:
+def relay3():
+  with open('/sys/class/gpio/gpio', pin3'/value') as gpio3:
+    var3 = int(gpio3.read())
+  if var3 == 0:
     print "on"
-  elif var23 == 1:
+  elif var3 == 1:
     print "off"
 
-def relay27():
-  with open('/sys/class/gpio/gpio27/value') as gpio27:
-    var27 = int(gpio27.read())
-  if var27 == 0:
+def relay4():
+  with open('/sys/class/gpio/gpio', pin4'/value') as gpio4:
+    var4 = int(gpio4.read())
+  if var4 == 0:
     print "on"
-  elif var27 == 1:
+  elif var4 == 1:
     print "off"
 
     
 if __name__ == "__main__":
-  print "relay18, Heater "
-  relay18()
-  print "relay22, Ballast Fan"
-  relay22()
-  print "relay23, Oscillating Fan"
-  relay23()
-  print "relay27, Main Light"
-  relay27()
+  relay1()
+  relay2()
+  relay3()
+  relay4()
