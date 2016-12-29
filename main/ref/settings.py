a28e3d3d62a5081c363d1092357bca30792649dc
scott snyder <scott@growmaster420.com>
2016-12-28 22:20:10 -0800

    Update settings.py

--- main/ref/settings.py
+++ main/ref/settings.py
@@ -17,3 +17,10 @@ def light():
     global lightOn, lightOff
     lightOn = 8
     lightOff = 20
+
+def expGpio():
+    global gp1, gp2, gp3, gp4
+    gp1 = "/sys/class/gpio/gpio18/value"
+    gp2 = "/sys/class/gpio/gpio22/value"
+    gp3 = "/sys/class/gpio/gpio23/value"
+    gp4 = "/sys/class/gpio/gpio27/value"
