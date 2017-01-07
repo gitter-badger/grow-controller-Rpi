1ab611a9fc8a8d6dddf665cc66801cccf103d0d1
scott snyder <scott@growmaster420.com>
2017-01-06 22:01:03 -0800

    Update watchdht.py

--- main/ref/watchdht.py
+++ main/ref/watchdht.py
@@ -10,7 +10,7 @@ pin5 = settings.dhtsensor
 
 while True:
   h,t = dht.read_retry(dht.DHT22, pin5) #read DHT22 value, set pi
-  # print(datetime.now().strftime("%H:%M:%S")) # python3 throws error on this line
+  print(time.strftime('%X %x %Z'))
   print('T={0:0.1f}*C\nH={1:0.1f}%'.format(t, h))
   time.sleep(8.0)
   os.system('clear')
