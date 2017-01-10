565d32bc08d22f6dec6d3512563998cb506ed185
growmaster420 <scott@growmaster420.com>
2017-01-09 22:55:04 -0800

    added alloff.py

--- main/menu.sh
+++ main/menu.sh
@@ -11,7 +11,7 @@ echo "︻╦╤─--------------------*==========================*--------------
 echo "! [1] apt-get u/g/d/a/i  ! [a] Light menu           ! [l] edit settings.py    !"
 echo "! [2] calculate V/A      ! [b] crontab maker        ! [m] SelectDefaultEditor !"
 echo "! [3] w & last           ! [c] GPIO state           ! [n] Reset I2C bus       !"
-echo "! [4] crontab -l         ! [d] Start LCD            ! [o]                     !"
+echo "! [4] crontab -l         ! [d] Start LCD            ! [o] All Relays Off      !"
 echo "! [5] crontab -e         ! [e] HDMI OFF             ! [p]                     !"
 echo "! [6] htop               ! [f] network info         ! [q]                     !"
 echo "! [7] processor temp     ! [g] watch DHT22          ! [r]                     !"
@@ -72,8 +72,10 @@ case $yourch in
         read yourch ;;
     m) python3 /home/pi/grow-controller-Rpi/main/ref/selectEditor.py && echo -n "Enter to continue"
         read yourch ;;
-    n) sudo modprobe -r i2c_bcm2708 && sudo modprobe i2c_bcm2708 baudrate=100010
-
+    n) sudo modprobe -r i2c_bcm2708 && sudo modprobe i2c_bcm2708 baudrate=100010 && echo -n "Enter to continue"
+        read yourch ;;
+    o) python3 /home/pi/grow-controller-Rpi/main/ref/alloff.py && echo -n "Enter to continue"
+        read yourch ;;
     0) exit 0 ;;
 *) echo "really?";
 echo "Press Enter to continue. . ." ; read ;;
