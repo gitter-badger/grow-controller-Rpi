7976f1799ede9025bd413212127b7133f44bef11
growmaster420 <scott@growmaster420.com>
2017-01-09 22:59:02 -0800

    fixed system updater

--- main/ref/update.sh
+++ main/ref/update.sh
@@ -1,18 +1,21 @@
+#!/usr/bin/env bash
 command -v apt >/dev/null 2>&1 || { echo "apt is not installed. not ubuntu?" >&2; exit 0; }
 echo "want to install anything?: [y/n]"  
 read yourch
-apt update
+sudo apt update
 if [ $yourch = "y" ];
-  then
-    echo "what program would u like to install"
-    read insch
-    apt install $insch -y
-    apt upgrade -y
-    apt dist-upgrade -y
-    apt autoremove -y
-else
-    apt upgrade -y
-    apt dist-upgrade -y
-    apt autoremove -y
+    then
+        echo "what program would u like to install"
+        read insch
+        sudo apt install $insch -y
+        sudo apt upgrade -y
+        sudo apt dist-upgrade -y
+        sudo apt autoremove -y
+fi
+if [ $yourch != "y" ];
+    then
+        sudo apt upgrade -y
+        sudo apt dist-upgrade -y
+        sudo apt autoremove -y
 fi
 exit
