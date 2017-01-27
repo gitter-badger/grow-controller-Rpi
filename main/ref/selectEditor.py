9fb8aaec4fd2f234f7eabb90a2b4ec986110494d
growmaster420 <scott@growmaster420.com>
2017-01-27 15:35:31 -0800

    lol

--- main/ref/selectEditor.py
+++ main/ref/selectEditor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys; print('Python %s on %s' % (sys.version, sys.platform))
 while True:
     print("Select Default Editor")
     print("1) emacs")
-    print("2) nano")
-    print("3) Vim/Vi")
+    print("2) nano   <----Recommended")
+    print("3) Vi     <----trolls love this editor")
     var1 = input("[1,2,3]:\n")
     if var1 != "2":
         print("try again")
