31123caf658c88c2f9de48f80b5c72192eb57aae
growmaster420 <scott@growmaster420.com>
2017-01-26 22:48:55 -0800

    changed var: "aio" to "web" its  more better

--- main/ref/web.py
+++ main/ref/web.py
@@ -9,12 +9,12 @@ settings.webgui()
 settings.pins()
 
 ADAFRUIT_IO_KEY = settings.key1
-aio = Client(ADAFRUIT_IO_KEY)
+web = Client(ADAFRUIT_IO_KEY)
 pin5 = settings.dhtsensor
 
 while True:
     h,t = dht.read_retry(dht.DHT22, pin5) #read DHT22 value, set pi
-    aio.send('Temp', t)
-    aio.send('Humd', h)
+    web.send('Temp', t)
+    web.send('Humd', h)
     time.sleep(10.0)
 
