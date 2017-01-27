7c33c87045af2fef5adb955834d97aa1b1857b5c
growmaster420 <scott@growmaster420.com>
2017-01-26 23:31:10 -0800

    testing

---
+++ main/test/web.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import Adafruit_DHT as dht
+import time
+import settings
+from Adafruit_IO import Client
+
+settings.webgui()
+settings.pins()
+
+ADAFRUIT_IO_KEY = settings.key1
+web = Client(ADAFRUIT_IO_KEY)
+pin5 = settings.dhtsensor
+
+while True:
+    h,t = dht.read_retry(dht.DHT22, pin5) #read DHT22 value, set pi
+    web.send('Temp', t)
+    web.send('Humd', h)
+    time.sleep(10.0)
+
