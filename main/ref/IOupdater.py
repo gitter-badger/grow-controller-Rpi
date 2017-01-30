13a012ffabc3ef28d50dfc35a98c55e594d4e783
growmaster420 <scott@growmaster420.com>
2017-01-30 03:25:30 -0800

    aIOupdater v0.0.1pre-alpha(test3)

--- main/ref/IOupdater.py
+++ main/ref/IOupdater.py
@@ -1,6 +1,7 @@
 
 import settings
 from Adafruit_IO import Client
+import gpioState
 
 
 settings.webgui()
@@ -16,10 +17,40 @@ settings.temps()
 temp1 = settings.maxTemp
 temp2 = settings.minTemp
 
-#main
-try:
-    web.send('timeOn', lightOn)
-    web.send('timeOff', lightOff)
-    web.send('lowTemp', temp2)
-    web.send('highTemp', temp1)
+###GPIOstate###
+gpioState.relay1()
+gpioState.relay2()
+gpioState.relay3()
+gpioState.relay4()
+gpstate1 = gpioState.state1  # ballast
+gpstate2 = gpioState.state2  # ballast fan
+gpstate3 = gpioState.state3  # heater
+gpstate4 = gpioState.state4  # ocfan
 
+
+#main
+web.send('timeOn', lightOn)
+web.send('timeOff', lightOff)
+web.send('lowTemp', temp2)
+web.send('highTemp', temp1)
+
+if gpstate1 == 0:
+    web.send('ballastState', OFF)
+if gpstate1 == 1:
+    web.send('ballastState', ON)
+'''
+if gpstate1 == 0:
+    web.send('', )
+if gpstate1 == 1:
+    web.send('', )
+
+if gpstate1 == 0:
+    web.send('', )
+if gpstate1 == 1:
+    web.send('', )
+
+if gpstate1 == 0:
+    web.send('', )
+if gpstate1 == 1:
+    web.send('', )
+'''
