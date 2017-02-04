28b2292917d802ad40b885ff03a67dc651642f75
growmaster420 <scott@growmaster420.com>
2017-02-03 19:44:13 -0800

    fix maybe

--- main/menu.sh
+++ main/menu.sh
@@ -43,7 +43,7 @@ case $yourch in
         read yourch ;;
     a) python3 /home/pi/grow-controller-Rpi/main/ref/relaymenu.py && echo -n "Enter to continue"
         read yourch ;;
-    b) python3 /home/pi/grow-controller-Rpi/main/ref/cronmaker.py > /home/pi/grow-controller-Rpi/main/ref/crontab.cron && echo -n "Enter to continue"
+    b) sudo python3 /home/pi/grow-controller-Rpi/main/ref/cronmaker.py > /home/pi/grow-controller-Rpi/main/ref/crontab.cron && echo -n "Enter to continue"
         read yourch ;;
     c) python3 /home/pi/grow-controller-Rpi/main/ref/gpioState.py && echo -n "Enter to continue"
         read yourch ;;
